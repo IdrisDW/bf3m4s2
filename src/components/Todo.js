ac263a1192eba96d6f73b722ec3c3dd20e4f70ff
Juliet <juguquro@gmail.com>
2021-10-06 20:24:56 -0700

    reto1

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../css/Todo.css";
+import Checkmark from './Checkmark';
+ import PropTypes from 'prop-types';
 
 class Todo extends React.Component {
   render() {
     return (
       <div className={`list-item ${this.props.task.done ? "done" : ""}`}>
         {this.props.task.content}
+        <div className="is-pulled-right">
+        <Checkmark done={this.props.done} />
+
+
         <button
           onClick={() => {
             // this.setState((prevState) => ({ done: !prevState.done }));
@@ -15,8 +21,14 @@ class Todo extends React.Component {
           style={{ height: 15, width: 10 }}
         />
       </div>
+      </div>
     );
   }
 }
+
+Todo.propTypes = {
+  done: PropTypes.bool,
+}
+
 
 export default Todo;
